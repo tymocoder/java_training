df2272c74c41ac63947cb3d22d5e2a3164114621
tymocoder <tymocoder@gmail.com>
2025-07-01 19:56:18 -0700

    Add hw02 part1. Annotations practice

---
+++ src/main/java/org/example/House.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import lombok.Builder;
+import lombok.Getter;
+
+// @Getter generates getter methods at compile time
+// @Builder allows to create objects using set-like chained methods
+@Getter
+@Builder
+public class House {
+    private String address;
+    private int rooms;
+    private double area;
+    private double price;
+}
